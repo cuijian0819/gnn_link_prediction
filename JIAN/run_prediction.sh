77616eb43f45d79845c24033b35b4b3ad29e567b
cuijian <32232191+cuijian0819@users.noreply.github.com>
2020-06-21 02:31:00 +0900

    fix shell codes and delete spark ver node2vec

--- JIAN/run_prediction.sh
+++ JIAN/run_prediction.sh
@@ -1,2 +1,3 @@
 python Main.py --data-name paper_author --test-unknown test_unknown.txt --hop 1 --only-predict && \
-python result_processing.py &&
+echo "saving private_answer.txt to prediction..."
+python result_processing.py
